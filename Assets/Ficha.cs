48ce5afd4982658c9c2da6600f6bdb882ccfb1ce
damayor <dr.mayorga20@uniandes.edu.co>
2019-10-30 15:59:09 -0500

    Ficha Instantiate

---
+++ Assets/Ficha.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ficha : MonoBehaviour {
+
+
+    protected Vector2 pos;
+
+    public Vector2 finalPos;
+
+    [SerializeField]
+    protected bool canMove;
+
+    //[SerializeField]
+    private Vector2 canvasPos;
+
+    protected bool isLocated;
+
+    //IsSelectedAs Touched
+
+    private bool isEmpty;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        //canvasPos = GetComponent<RectTransform>().anchoredPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+
+    private Vector2 GetPosition()
+    {
+        return canvasPos;
+
+    }
+
+    public bool IsEmptySpace()
+    {
+        return isEmpty;
+    }
+
+    public void SetAsEmpty()
+    {
+        isEmpty = true;
+    }
+    
+
+
+
+}
---
+++ Assets/Puzzle.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Puzzle : MonoBehaviour
+{
+
+
+    private int sizeX;
+
+    private int sizeY;
+
+    private Ficha[] fichas;
+
+    public GameObject fichaPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        sizeX = 3;
+        sizeY = 3;
+
+        PopulatePuzzle();
+
+        fichas = GetComponentsInChildren<Ficha>();
+
+
+        //Desordenar
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Genera el puzzle y asigna las variables de las fichas
+    void PopulatePuzzle()
+    {
+
+        Ficha newFicha;
+        int fichaImg = 1;
+
+        for (int j = 0; j < sizeX; j++)
+        {
+
+            for (int i = 0; i < sizeY; i++)
+            {
+
+
+                newFicha = fichaPrefab.GetComponent<Ficha>();
+
+                newFicha.finalPos = new Vector2(i, j);
+
+                if (i == sizeX - 1 && j == sizeY - 1)
+                {
+                    newFicha.GetComponentInChildren<Text>().text = "_";
+                    newFicha.SetAsEmpty();
+                }
+
+                fichaPrefab.GetComponentInChildren<Text>().text = fichaImg + "";
+                fichaImg++;
+
+                Instantiate(fichaPrefab, gameObject.transform);
+            }
+
+        }
+
+    }
+}
