db02b6150000c936b7a367aa5ec183e877c2e52f
damayor <dr.mayorga20@uniandes.edu.co>
2019-11-01 13:12:19 -0500

    Randomize positions

--- Assets/Ficha.cs
+++ Assets/Ficha.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ficha : MonoBehaviour {
 
 
-    protected Vector2 pos;
+    public Vector2 pos;
 
     public Vector2 finalPos;
 
@@ -13,45 +14,159 @@ public class Ficha : MonoBehaviour {
     protected bool canMove;
 
     //[SerializeField]
-    private Vector2 canvasPos;
+    //public Vector3 canvasPos;
 
     protected bool isLocated;
 
-    //IsSelectedAs Touched
+    public bool isRandomed;
 
-    private bool isEmpty;
+    private bool isTap;
+
+    public bool isEmpty = false;
+
+    public Puzzle puzzleInfo;
 
 
+    
     // Use this for initialization
     void Start ()
     {
+        //canvasPos = GetComponent<RectTransform>().rect.center;
+        //pff solo canvas Debug.Log(GetComponent<RectTransform>().anchoredPosition3D + ";;;" + GetComponent<RectTransform>().anchoredPosition);
 
-        //canvasPos = GetComponent<RectTransform>().anchoredPosition;
-
+        //anchoredposition No..
     }
 
     // Update is called once per frame
     void Update ()
     {
-		
-	}
+        // Debug.Log(GetComponent<RectTransform>().anchoredPosition3D + ";;;" + GetComponent<RectTransform>().anchoredPosition);
 
-    private Vector2 GetPosition()
-    {
-        return canvasPos;
-
+        //if (isTap)
+        //{
+        //    MoveToSpace(f);
+        //}
     }
+
+    //private Vector2 GetPosition()
+    //{
+    //    return canvasPos;
+
+    //}
 
     public bool IsEmptySpace()
     {
         return isEmpty;
     }
 
-    public void SetAsEmpty()
+    public void SetAsEmpty(bool b)
     {
-        isEmpty = true;
+        isEmpty = b;
     }
-    
+
+    public void SetFinalPos(Vector2 v)
+    {
+        finalPos = v;
+    }
+
+    public Vector2 GetFinalPos()
+    {
+        return finalPos;
+    }
+
+    public void SetPos(Vector2 v)
+    {
+        pos = v;
+    }
+
+    public Vector2 GetPos()
+    {
+        return pos;
+    }
+
+    //Se puede mover porque tiene el espacio al lado
+    public bool CanIMove(Ficha espacio)
+    {
+
+        if (espacio.pos.y == pos.y
+                && Mathf.Abs(pos.x - espacio.pos.x) == 1)
+        {
+            canMove = true;
+        }
+        else if (espacio.pos.x == pos.x
+                && Mathf.Abs(pos.y - espacio.pos.y) == 1)
+        {
+            canMove = true;
+        }
+        else
+        {
+            canMove = false;
+        };
+
+        return canMove;
+
+    }
+
+
+    public void MoveToSpace(Ficha espacio)
+    {
+
+        //Ficha espacio = puzzleInfo.GetEmptyFicha();
+
+        if (CanIMove(espacio))
+        {
+            Vector2 spacePos = espacio.pos;
+            espacio.pos = pos;
+            pos = spacePos;
+
+            //y lo mismo con XYZ location
+                Vector3 empty3DLocation = espacio.transform.position;
+                Vector3 my3DLocation = transform.position;
+
+                transform.position = empty3DLocation; //yeahh
+            //no cambia!! Ni en 3D
+            espacio.transform.position = my3DLocation;
+           
+
+            //Puzzle.CheckMovables, CheckIfPuzzleDone
+
+            Debug.Log("Movido el "+ GetComponentInChildren<TextMesh>().text +" al espacio "+ my3DLocation);
+        }
+
+    }
+
+
+    public void ChangePos(Vector3 p)
+    {
+        //   transform.GetComponent<RectTransform>().anchoredPosition.Set(p.x, p.y);
+        transform.position = p;
+    }
+
+
+    public void SetTap()
+    {
+        isTap = true;
+    }
+
+    //public void Move2D(Ficha espacio)
+    //{
+    //    Vector2 spacePos = espacio.pos;
+    //    espacio.pos = pos;
+    //    pos = spacePos;
+
+    //    //y lo mismo con XYZ location
+    //    Vector2 empty3DLocation = espacio.GetComponent<RectTransform>().anchoredPosition;
+    //    Vector2 my3DLocation = GetComponent<RectTransform>().anchoredPosition;
+
+    //    GetComponent<RectTransform>().anchoredPosition = empty3DLocation; //yeahh
+    //                                                                      //no cambia!!
+    //    espacio.ChangePos(new Vector2(300, 100)); //nada...
+
+    //    //Puzzle.CheckMovables, CheckIfPuzzleDone
+
+    //    Debug.Log("Movido el " + GetComponentInChildren<Text>().text + " al espacio " + my3DLocation);
+    //}
+
 
 
 
--- Assets/Puzzle.cs
+++ Assets/Puzzle.cs
@@ -11,30 +11,67 @@ public class Puzzle : MonoBehaviour
 
     private int sizeY;
 
-    private Ficha[] fichas;
+    public Ficha[] fichas;
+
+    public Ficha[,] fichasArray;
+
 
     public GameObject fichaPrefab;
+
+    private Ficha emptyFicha;
+
+    public float height;
+    public float width;
+    public float spaceX;
+    public float spaceY;
+    public float zeroX;
+    public float zeroY;
+    public Vector3 initFichaPos = new Vector3(2, 2, 0);
 
     // Use this for initialization
     void Start()
     {
 
-
         sizeX = 3;
         sizeY = 3;
 
+        fichas = new Ficha[sizeX * sizeY];
+
+        fichasArray = new Ficha[sizeX, sizeY];
+
         PopulatePuzzle();
 
-        fichas = GetComponentsInChildren<Ficha>();
+        //fichas = GetComponentsInChildren<Ficha>();
 
-
-        //Desordenar
     }
 
     // Update is called once per frame
     void Update()
     {
+        //for (int i = 0; i < fichas.Length; i++)
+        //{
+        //    fichas[i].CanIMove(emptyFicha);
+        //}
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            Camera cam = Camera.main;
+            Vector3 posMouse = Input.mousePosition;
+            Ray raycast = cam.ScreenPointToRay(posMouse);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                Transform objectHit = raycastHit.transform;
+
+                objectHit.SendMessage("MoveToSpace", emptyFicha);
+            }
+            else
+            {
+                Debug.Log("RELA, nada tocado");
+            }
+
+        }
     }
 
     //Genera el puzzle y asigna las variables de las fichas
@@ -42,32 +79,139 @@ public class Puzzle : MonoBehaviour
     {
 
         Ficha newFicha;
+
         int fichaImg = 1;
+        Vector3 canvasPos = initFichaPos;
 
-        for (int j = 0; j < sizeX; j++)
+
+        for (int i = 0; i < sizeY; i++)
         {
-
-            for (int i = 0; i < sizeY; i++)
+            for (int j = 0; j < sizeX; j++)
             {
+                GameObject go = Instantiate(fichaPrefab, canvasPos, Quaternion.Euler(-90, 0, 0), this.transform) as GameObject;
 
-
-                newFicha = fichaPrefab.GetComponent<Ficha>();
-
+                newFicha = go.GetComponent<Ficha>();
                 newFicha.finalPos = new Vector2(i, j);
+                newFicha.pos = new Vector2(i, j);
+                go.GetComponentInChildren<TextMesh>().text = fichaImg + "";
 
                 if (i == sizeX - 1 && j == sizeY - 1)
                 {
-                    newFicha.GetComponentInChildren<Text>().text = "_";
-                    newFicha.SetAsEmpty();
+                    newFicha.SetAsEmpty(true);
+                    newFicha.GetComponentInChildren<TextMesh>().text = "_";
+                    emptyFicha = newFicha;
+                }
+                //si no específica una asignacion en un prefab, lo aplica a todos
+                else
+                {
+                    newFicha.SetAsEmpty(false);
+                    newFicha.GetComponentInChildren<TextMesh>().text = fichaImg + "";
                 }
 
-                fichaPrefab.GetComponentInChildren<Text>().text = fichaImg + "";
-                fichaImg++;
+                newFicha.puzzleInfo = this;
 
-                Instantiate(fichaPrefab, gameObject.transform);
+                fichas[fichaImg - 1] = newFicha;
+                fichasArray[i, j] = newFicha;
+                fichaImg++;
+                canvasPos.x = canvasPos.x + width + spaceX;
+
+            }
+
+            canvasPos.y = canvasPos.y + height + spaceY;
+            canvasPos.x = initFichaPos.x;
+        }
+    }
+
+    void MoveToEmptySpace(Vector2 f)
+    {
+
+
+    }
+
+    public Ficha GetEmptyFicha()
+    {
+        return emptyFicha;
+    }
+
+
+
+    public void DesordenarPuzzle()
+    {
+        //ficha por ficha y m lo pone en otra pos
+        //esa pos queda chuleada
+        bool[,] located = new bool[sizeX, sizeY];
+        int tries = 0;
+
+        Vector3[,] posBacks = new Vector3[sizeX, sizeY];
+
+        //guarde la posicion de cada ficha
+        for (int s = 0; s< sizeY; s++)
+        {
+            for (int t = 0; t < sizeY; t++)
+            { 
+                posBacks[s,t] = fichasArray[s,t].transform.position;
+            }
+        }
+
+        for (int i = 0; i < sizeY; i++)
+        {
+            for (int j = 0; j < sizeX; j++)
+            {
+
+                int h = Random.Range(0, sizeX );
+                int v = Random.Range(0, sizeY );
+
+                while ( !fichasArray[i,j].isRandomed)
+                {
+                    if (!located[h, v])
+                    {
+
+                        located[h, v] = true;
+                        fichasArray[i, j].transform.position = posBacks[h, v];
+
+                        fichasArray[i, j].pos = new Vector2(h, v);
+                        fichasArray[i, j].isRandomed = true;                       
+
+                    }
+                    else
+                    {
+                        h = Random.Range(0, sizeX );
+                        v = Random.Range(0, sizeY );
+                        //vuelva al while
+                    }
+
+                    tries++;
+                }
+
+            }
+        }
+
+        Debug.Log("puzzle desordenado despues de " + tries + " intentos.");
+    }
+
+    public bool CheckWon()
+    {
+        foreach (Ficha f in fichasArray)
+        {
+
+            if (f.pos == f.finalPos)
+            {
+
+                Debug.Log("Yei la ficha " + f.finalPos+" esta ubicada.");
+                return true;
+            }
+            else
+            {
+                Debug.Log("Uy todavia falta la ficha " + f.finalPos);
+                return false;
             }
 
         }
 
+        return false ;
+
     }
+
+
 }
+
